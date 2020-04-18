4cad84db20dc5c6b8f1957ae5770507fde2db084
Dan <danvernon@gmail.com>
2020-04-19 00:41:13 +0100

    Base views init

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,3 @@
-# Configure your routes here
-# See: https://guides.hanamirb.org/routing/overview
-#
-# Example:
-# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+# apps/web/config/routes.rb
+root to: 'home#index'
+get '/books', to: 'books#index'
